645c092aaaad24355211beaacf09420022c89aae
4boysdontcry <hgy02166@gmail.com>
2021-04-23 17:58:24 +0900

    sub-navi <matches>까지 만듬

--- js/index.js
+++ js/index.js
@@ -5,7 +5,14 @@
 
 
 /*************** 이벤트 등록 *****************/
-
+$('.bot-header .bot-navi').on('mouseenter', onShowSubNavi)
+$('.sub-navi-wrapper').on('mouseleave', onHideSubNavi)
 
 /*************** 이벤트 콜백 *****************/
+function onShowSubNavi(){
+    $('.news').show()
+}
 
+function onHideSubNavi(){
+    $('.sub-navi-wrapper').hide()
+}
