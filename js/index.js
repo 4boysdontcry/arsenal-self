d11ad82eae70187b3f313592d2e77f728b9f7b14
4boysdontcry <hgy02166@gmail.com>
2021-04-24 18:39:50 +0900

    js 죽겠어요...

--- js/index.js
+++ js/index.js
@@ -1,22 +1,30 @@
 /*************** 글로벌 설정 *****************/
-
+var naviLen = $('.navi').index
+console.log(naviLen)
+// console.log($(navi[0]))  // []안은 length값을 넣어 호출 가능 (0 ~ n)
 
 /*************** 사용자 함수 *****************/
-var subMenu = $('.sub-menu')
-console.log(subMenu.index())
 
-/*************** 이벤트 등록 *****************/
-// var naviIdx = $('.bot-header .navi').index()
+// var navi = $('.bot-header .navi')
+// var subNavi = $('sub-menu .sub-navi-wrapper')
+// var subIdx = each('.sub-navi-wrapper').length[each]
+
+var navi = $('.navi')
+var idx = navi.index()
+console.log($(navi.idx()))
 
 
-// $('.bot-header .bot-navi').on('mouseenter', onShowSubNavi)
-// $('.sub-navi-wrapper').on('mouseleave', onHideSubNavi)
+// /*************** 이벤트 등록 *****************/
+// $(navi[idx]).on('mouseenter', showSubNavi)
+// $(subNavi[subIdx]).on('mouseleave', hideSubNavi)
+
 
 // /*************** 이벤트 콜백 *****************/
-// function onShowSubNavi(){
-//     $('.news').show()
+// function showSubNavi(){
+//     subNavi[subIdx].show()
 // }
 
-// function onHideSubNavi(){
+// function hideSubNavi(){
 //     $('.sub-navi-wrapper').hide()
 // }
+
