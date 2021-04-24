abf58cd2698ebdeb956cc90f30e40ff4891fa851
4boysdontcry <hgy02166@gmail.com>
2021-04-24 16:57:52 +0900

    sub-menu완성

--- js/index.js
+++ js/index.js
@@ -2,17 +2,21 @@
 
 
 /*************** 사용자 함수 *****************/
-
+var subMenu = $('.sub-menu')
+console.log(subMenu.index())
 
 /*************** 이벤트 등록 *****************/
-$('.bot-header .bot-navi').on('mouseenter', onShowSubNavi)
-$('.sub-navi-wrapper').on('mouseleave', onHideSubNavi)
+// var naviIdx = $('.bot-header .navi').index()
 
-/*************** 이벤트 콜백 *****************/
-function onShowSubNavi(){
-    $('.news').show()
-}
 
-function onHideSubNavi(){
-    $('.sub-navi-wrapper').hide()
-}
+// $('.bot-header .bot-navi').on('mouseenter', onShowSubNavi)
+// $('.sub-navi-wrapper').on('mouseleave', onHideSubNavi)
+
+// /*************** 이벤트 콜백 *****************/
+// function onShowSubNavi(){
+//     $('.news').show()
+// }
+
+// function onHideSubNavi(){
+//     $('.sub-navi-wrapper').hide()
+// }
