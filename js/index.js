52a66d6902d2f1c0ef1f9d2966ea841f68e39751
4boysdontcry <hgy02166@gmail.com>
2021-05-04 21:26:56 +0900

    swiper 붙이는중

--- js/index.js
+++ js/index.js
@@ -16,13 +16,13 @@ console.log(lastIdx)
 
 
 /*************** 사용자 함수 *****************/
+slideShop();
 
 
 
 // /*************** 이벤트 등록 *****************/
 $navi.on('mouseenter', onShowSubNavi)
 $subNavi.on('mouseleave', onHideSubNavi)
-
 
 // /*************** 이벤트 콜백 *****************/
 function onShowSubNavi(){
@@ -36,4 +36,29 @@ function onShowSubNavi(){
 function onHideSubNavi(){
     $subNavi.hide()
     $navi.css('color','#fff').removeClass('active')
+}
+
+
+function slideShop() {
+    var $shop = $('.shop-wrapper');
+    var $slideWrapper = $shop.find('.slide-wrapper');
+    
+    function onGetData(r) {
+        r.shop.forEach(function (v, i) {
+            var html = '';
+            html += '<li class="slide">';
+            html += '<div class="img-wrap">';
+            html += '<img src="' + v.src + '" alt="의류" class="w-100">';
+            html += '</div>';
+            html += '<div class="desc-wrap">';
+            html += '<h4 class="subject">'+ v.subject +' </h4>';
+            html += '<h3 class="title">'+ v.title +' </h3>';
+            html += '<p class="price">'+ v.price +' </p>';
+            html += '</div>';
+            html += '</li>';
+            $slideWrapper.append(html);
+        })
+        var swiper = getSwiper('.shop-wrapper', { break: 3, speed: 600 });
+		}
+    $.get('../json/shop.json', onGetData);
 }
